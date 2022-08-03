66ae5c6573dd18ea77c0b121718c2aa6b87004b3
Aaron Hellam <aaronhellam@gmail.com>
2022-08-02 17:27:57 -0700

    scrollable player divs and starting selected player containers

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,8 +1,14 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :rank, :position_id, :price, :canDelete, :games
+  attributes :id, :name, :image, :rank, :position_id, :pos, :price, :canDelete, :games
   belongs_to :position
   
   def games 
     object.games
   end
+
+  def pos 
+    positions = [nil, 'QB', 'WR', 'RB']
+    positions[object.position_id]
+  end
+
 end
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -10,24 +10,36 @@ const Home = ({ positions }) => {
   const [selectedRB, setSelectedRB] = useState(null)
 
   return (
-  <div>
-    <div className="mt-2 p-1 text-xl font-semibold text-center">
+  <div className="">
+    <div className="mt-2 p-1 text-xl bg-white font-semibold text-center sticky top-16">
       <p>Remaining Balance: <span className="text-green-500">{`$${balance}`}</span></p>
+      {/* CREATE EMPTY CONTAINERS FOR SELETCED PLAYERS HERE...... */}
+      <div id="selected-players" className="m-1 grid grid-flow-col justify-items-center">
+        <div className="m-1 p-1 rounded-md bg-[#323236] text-white grid grid-cols-3">
+          qb
+        </div>
+        <div className="m-1 p-1 rounded-md bg-[#323236] text-white grid grid-cols-3">
+          wr
+        </div>
+        <div className="m-1 p-1 rounded-md bg-[#323236] text-white grid grid-cols-3">
+          rb
+        </div>
+      </div>
     </div>
     <div id="positions-containter" className="m-1 grid grid-flow-col justify-items-center">
-      <div id="qb-container">
+      <div id="qb-container" className="overflow-y-auto h-2/5">
         {positions && positions.find(position => position.id === 1)
         .players.map(quarterback => (
           <Quarterbacks key={quarterback.id} quarterback={quarterback}/>
         ))}
       </div>
-      <div id="wr-container">
+      <div id="wr-container" className="overflow-y-auto h-2/5">
         {positions && positions.find(position => position.id === 2)
         .players.map(reciever => (
           <Recievers key={reciever.id} reciever={reciever}/>
         ))}
       </div>
-      <div id="rb-container">
+      <div id="rb-container" className="overflow-y-auto h-2/5">
         {positions && positions.find(position => position.id === 3)
         .players.map(runningback => (
           <Runningbacks key={runningback.id} runningback={runningback}/>
